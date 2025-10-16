d7945fe5eac0a99110811fb9bf2dd07d21fae18d
maisamarall <maisaamaral220@gmail.com>
2025-10-15 21:27:45 -0300

    atualizando projeto

--- cypress/e2e/API/api_automationexercise.cy.js
+++ cypress/e2e/API/api_automationexercise.cy.js
@@ -1,45 +1,47 @@
 describe('Funcionalidade: Testes de API - Automation Exercise', () => {
 
-    // retornar lista de produtos
-    it('Cenário: Listagem dos produtos - Deve retornar a lista de produtos com sucesso', () => {
-        cy.request('GET', 'https://automationexercise.com/api/productsList').then((response) => {
-            expect(response.status).to.equal(200);
+    const baseUrl = 'https://automationexercise.com/api';
+
+    // Cenário: Listar produtos
+    it("Dado que quero listar produtos, quando der GET /productsList, então retornar status 200", () => {
+        cy.request("GET", `${baseUrl}/productsList`).then((response) => {
+            expect(response.status).to.eq(200);
+            // const body = JSON.parse(response.body);
+            const body = response.body;
+
+            expect(response.status).to.eq(200);
+            expect(body).to.have.property('products');
         });
     });
 
-    // retornar status 405 e mensagem de erro VERIFICAR
-    it('Cenário: POST /api/productsList - Deve retornar status 405 E mensagem de erro (método não permitido)', () => {
+    // Cenário: Retornar erro 405 e mensagem de erro 
+    it('POST /productsList - Deve retornar status 405 e mensagem de erro para POST', () => {
         cy.request({
             method: 'POST',
-            url: 'https://automationexercise.com/api/productsList',
+            url: `${baseUrl}/productsList`,
         }).then((response) => {
-
-            const responseJson = JSON.parse(response.body);
-            expect(response.status).to.equal(200);
-            expect(responseJson).to.have.property('responseCode', 405);
-            expect(responseJson.responseCode).to.equal(405);
+            expect(response.status).to.equal(405);
+            // const body = JSON.parse(response.body);
+            expect(body).to.have.property('message');
+            expect(body.message).to.include('This request method is not supported')
         });
     });
 
-    // retornar lista de marcas
-    it('Cenário: GET /api/brandsList - Deve retornar lista de marcas e status 200', () => {
-        cy.request('GET', 'https://automationexercise.com/api/brandsList').then((response) => {
+    // Cenário: Listar marcas
+    it('Dado que faço um GET em /brandsList, então deve retornar lista de marcas e status 200', () => {
+        cy.request('GET', `${baseUrl}/brandsList`).then((response) => {
             expect(response.status).to.equal(200);
+            const body = response.body;
 
-            const responseJson = JSON.parse(response.body);
-            expect(responseJson).to.have.property('brands');
-            expect(responseJson.brands).to.be.an('array');
-            expect(responseJson.brands.length).to.be.greaterThan(0);
-
-            const firstBrand = responseJson.brands[0];
-            expect(firstBrand).to.have.all.keys('id', 'brand');
-            expect(firstBrand.brand).to.be.a('string');
+            // const body = JSON.parse(response.body);
+            expect(body).to.have.property('brands');
+            expect(body.brands).to.be.an('array');
         });
     });
 
-    //Buscar produto válido 
-    it('Cenário: POST /api/searchProduct - Deve buscar por produto válido e validar resposta com status (200)', () => {
-        const searchTerm = 'blue top';
+    // Cenário: Buscar produto válido
+    it('Dado que envio um produto válido no POST /searchProduct, então deve retornar status 200', () => {
+        const searchTerm = 'top';
 
         cy.request({
             method: 'POST',
@@ -49,45 +51,65 @@ describe('Funcionalidade: Testes de API - Automation Exercise', () => {
                 search_product: searchTerm
             }
         }).then((response) => {
-            expect(response.status).to.equal(200);
+            const body = response.body;
+            expect(body).to.have.property('products')
+            expect(response.status).to.eq(200);
+            expect(response.body.products).to.be.an('array');
         });
     });
 
-    //Enviar sem parâmetro search_product
-    it('Cenário: POST /api/searchProduct - Sem parâmetro deve retornar erro (status 400)', () => {
+    // Cenário: Buscar sem parâmetro
+    it('Dado que envio POST /searchProduct sem parâmetro, então deve retornar erro com responseCode 400', () => {
         cy.request({
             method: 'POST',
-            url: 'https://automationexercise.com/api/searchProduct',
+            url: `${baseUrl}/searchProduct`,
             form: true,
             body: {}
         }).then((response) => {
             expect(response.status).to.equal(200);
-            const responseJson = JSON.parse(response.body);
-            expect(responseJson).to.have.property('responseCode', 400);
+            const body = JSxxON.parse(response.body);
+            expect(body).to.have.property('responseCode', 400);
         });
     });
 
-    //Validar login com dados válidos (status 200) e inválidos (status 404) VERIFICAR
-    it('Cenário: POST /api/verifyLogin - Validar login com dados válidos (status 200)', () => {
-
+    // Cenário: Login com dados válidos (sucesso)
+    it('Dado que envio dados válidos no POST /verifyLogin, então deve retornar status 200 e mensagem de sucesso', () => {
         cy.request({
             method: 'POST',
             url: 'https://automationexercise.com/api/verifyLogin',
             form: true,
             body: {
-                email: email,
-                password: senha
+                email: 'ms.usuarioo@gmail.com',
+                password: 'senha123'
             }
         }).then((response) => {
-            const responseJson = JSON.parse(response.body);
-            expect(responseJson.responseCode).to.equal(200);
-            expect(responseJson.message).to.include('User exists!');
-        });
+            expect(response.status).to.eq(200);
 
+            const body = JSON.parse(response.body);
+
+            expect(body.responseCode).to.eq(200);
+            expect(body.message).to.include('User exists!');
+        });
     });
 
-    // Criar usuário
-    it('Cenário: POST /api/createAccount - Criar novo usuário e validar mensagem de sucesso (status 200)', () => {
+    // Cenário: Login com dados válidos (erro)
+    it('Dado que envio dados válidos no POST /verifyLogin, então deve retornar status 200 e mensagem de sucesso', () => {
+        const user = {
+            email: 'ms.usuario@gmail.com',
+            password: 'senha123'
+        }
+        cy.request({ method: "POST", url: `${baseUrl}/verifyLogin`, form: true, body: user }).then((response) => {
+            expect(response.status).to.eq(200);
+
+            const body = JSON.parse(response.body);
+
+            expect(body.responseCode).to.eq(200);
+            expect(body.message).to.include("User exists!");
+        })
+    });
+
+    // Cenário: Criar novo usuário
+    it('Dado que envio dados válidos no POST /createAccount, então o usuário deve ser criado com sucesso', () => {
         const email = `ms.usuario${Date.now()}@gmail.com`;
 
         cy.request({
@@ -96,7 +118,7 @@ describe('Funcionalidade: Testes de API - Automation Exercise', () => {
             form: true,
             body: {
                 name: 'TesteCypress',
-                email: 'ms.usuario@gmail.com',
+                email: email,
                 password: 'senha123',
                 title: 'MS',
                 birth_date: '22',
@@ -120,8 +142,8 @@ describe('Funcionalidade: Testes de API - Automation Exercise', () => {
         });
     });
 
-    //Deletar
-    it('Cenário: DELETE /api/deleteAccount - Deve deletar o usuário com sucesso (status 200)', () => {
+    // Cenário: Deletar usuário
+    it('Dado que envio dados válidos no DELETE /deleteAccount, então o usuário deve ser deletado com sucesso', () => {
         const email = 'ms.usuario@gmail.com';
         const password = 'senha123';
 
